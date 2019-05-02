d7007a30a12e820f05d6b31a7eed3ba9304f805e
Mr5he11 <Malmsteen97>
2019-05-02 22:21:44 +0200

    Added mongoose schema for user

---
+++ api/models/user.ts
@@ -0,0 +1,25 @@
+import { Document, Schema, Model, model} from "mongoose";
+
+const Roles = Object.freeze({
+	Cook: "cook",
+	Waiter: "waiter",
+	CashDesk: "cash_desk"
+});
+
+export interface IUser extends Document {
+  username: string;
+  digest: string;
+  salt: string;
+  role: string;
+}
+
+const UserSchema: Schema = new Schema({
+	username: { type: String, required: true, unique: true },
+	digest: { type: String, required: true },
+	salt: { type: String, required: true },
+	role: { type: String, enum: Object.values(Roles) }
+});
+
+Object.assign(UserSchema.statics, { Roles });
+
+export default model<IUser>('User', UserSchema);
