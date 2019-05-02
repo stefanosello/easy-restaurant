82ce1d48d8ebf790e9a9151005150e199cf01c76
Mr5he11 <Malmsteen97>
2019-05-02 20:28:52 +0200

    Added express middleware for statics

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello World!");
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('./easy-restaurant-frontend/dist/easy-restaurant-frontend'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
