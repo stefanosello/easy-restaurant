deed7499aaedf043f890ac440d28f27737dae9d7
Stefano Sello <ssello@shado.tv>
2019-05-23 21:30:44 +0200

    [CQ]

--- backend/src/models/table.ts
+++ backend/src/models/table.ts
@@ -4,9 +4,9 @@ import { IOrder, OrderSchema } from "./order";
 export const ServiceSchema: Schema = new Schema({
 	covers: { type: Number, required: true },
 	waiter: { type: Schema.Types.ObjectId, required: true, ref: "User" },
-    orders: [OrderSchema],
-    timestamp: { type: Date },
-    done: { type: Boolean, default: false }
+	orders: [OrderSchema],
+	timestamp: { type: Date },
+	done: { type: Boolean, default: false }
 });
 
 export interface ITable extends Document {
