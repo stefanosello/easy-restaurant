94857ecbd3f3ad073875e0364b5d5b711e32cab4
Stefano Sello <sellostefano@live.it>
2019-06-24 12:33:37 +0200

    Some work on items tree

--- frontend/src/app/waiter/waiter-order-modal/waiter-order-modal.component.ts
+++ frontend/src/app/waiter/waiter-order-modal/waiter-order-modal.component.ts
@@ -5,7 +5,13 @@ import { FormControl, Validators } from '@angular/forms';
 import { Table } from 'src/app/_models/table';
 import { AuthService } from 'src/app/_services/auth.service';
 import { foodOrders, beverageOrders, pendingBeverageOrders, pendingFoodOrders, processedFoodOrders, processedBeverageOrders } from '../../_helpers/table-helper';
+import { Item } from 'src/app/_models/order';
 
+export interface ItemNode {
+  name: string;
+  quantity: number;
+  children?: ItemNode[];
+}
 
 @Component({
   selector: 'app-waiter-order-modal',
@@ -36,6 +42,34 @@ export class WaiterOrderModalComponent implements OnInit {
 
   public onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  private formatOrdersForTree(): ItemNode[] {
+    let result: ItemNode[] = [];
+    let foodOrders: ItemNode = {
+      name: "Food Orders",
+      quantity: this.foodOrders(this.table).length,
+      children: []
+    }
+    let BeverageOrders: ItemNode = {
+      name: "Beverage Orders",
+      quantity: this.beverageOrders(this.table).length,
+      children: []
+    }
+    this.beverageOrders(this.table).forEach((order, index) => {
+      let formattedOrder: ItemNode = {
+        name: `Orderd #${index}`,
+        quantity: order.items.length,
+        children: []
+      };
+      order.items.forEach((item: Item) => {
+        formattedOrder.children.push({
+          name: item.name,
+          quantity: item.quantity
+        })
+      })
+    });
+    return result;
   }
 
 }
