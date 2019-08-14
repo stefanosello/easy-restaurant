9911f38c95afa595044a56c409415603cfa3c936
Stefano Sello <ssello@shado.tv>
2019-08-14 11:13:18 +0200

    Some work done on order tree component

--- frontend/src/app/waiter/waiter-order-modal/order-tree/order-tree.component.ts
+++ frontend/src/app/waiter/waiter-order-modal/order-tree/order-tree.component.ts
@@ -3,38 +3,6 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MatTreeNestedDataSource } from '@angular/material/tree';
 import { ItemNode } from '../waiter-order-modal.component';
 
-const TREE_DATA: ItemNode[] = [
-  {
-    name: 'Fruit',
-    children: [
-      { name: 'Apple', quantity: 3 },
-      { name: 'Banana', quantity: 3 },
-      { name: 'Fruit loops', quantity: 3 },
-    ],
-    quantity: 9
-  }, {
-    name: 'Vegetables',
-    children: [
-      {
-        name: 'Green',
-        children: [
-          { name: 'Broccoli', quantity: 7 },
-          { name: 'Brussel sprouts', quantity: 7 },
-        ],
-        quantity: 14
-      }, {
-        name: 'Orange',
-        children: [
-          { name: 'Pumpkins', quantity: 8 },
-          { name: 'Carrots', quantity: 8 },
-        ],
-        quantity: 16
-      },
-    ],
-    quantity: 32
-  },
-];
-
 @Component({
   selector: 'app-order-tree',
   templateUrl: './order-tree.component.html',
@@ -42,10 +10,16 @@ const TREE_DATA: ItemNode[] = [
 })
 export class OrderTreeComponent implements OnInit {
 
+  // tslint:disable-next-line: no-input-rename
   @Input('tree') tree: ItemNode;
 
   treeControl = new NestedTreeControl<ItemNode>(node => node.children);
   dataSource = new MatTreeNestedDataSource<ItemNode>();
+  addingNewItem = false;
+  newItem = {
+    quantity: 1,
+    name: ''
+  };
 
   constructor() {
   }
