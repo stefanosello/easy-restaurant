ac08a332cec5dd130003f1628d97967fb2ffb339
Stefano Sello <ssello@shado.tv>
2019-09-14 16:40:31 +0200

    Cordova now working

--- EasyRestaurantMobileApp/install.sh
+++ EasyRestaurantMobileApp/install.sh
@@ -1,4 +1,5 @@
 cd ../frontend && rm -r dist && ng build --configuration cordova
-cd ../EasyRestaurantMobileApp && rm -r ./www/* && cp ../frontend/dist/easy-restaurant-frontend/* ./www/ -r
+cd ../EasyRestaurantMobileApp && rm -r ./www/*
+cp ../frontend/dist/easy-restaurant-frontend/* ./www/ -r
 cordova build android
 cordova emulate android
